1661452e22b523ac66ccdf4d7a3f6a6095e3cb40
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-07-24 02:08:36 +0700

    feat(book-module): add repository

---
+++ src/modules/books/repository/index.ts
@@ -0,0 +1,33 @@
+import {BookCreationAttributes, BookInstance, BookUpdateAttributes} from '../model/type';
+import {BookModel} from '../model';
+
+class BookRepository {
+    public async getAll(): Promise<BookInstance[]> {
+        return await BookModel.findAll();
+    }
+
+    public async getById(id: number): Promise<BookInstance> {
+        const book = await BookModel.findById(id);
+        if (!book) {
+            throw new Error('Book not found');
+        }
+
+        return book;
+    }
+
+    public async create(data: BookCreationAttributes): Promise<BookInstance> {
+        return await BookModel.create(data);
+    }
+
+    public async update(id: number, data: BookUpdateAttributes): Promise<BookInstance> {
+        const book = await this.getById(id);
+        return await book.update(data);
+    }
+
+    public async delete(id: number): Promise<void> {
+        const book = await this.getById(id);
+        return await book.destroy();
+    }
+}
+
+export const bookRepository = new BookRepository();
