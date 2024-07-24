67c6b7684ed6dffc6809b9fc51a5a505abb98362
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-07-24 13:01:13 +0700

    feat(server): add checkMainAdmin method

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,12 @@
 import {pgConnection} from './connections/postgresConnection';
-import {startServer} from './server';
+import {checkMainAdmin, startServer} from './server';
 import {UserModel} from './modules/users/model';
 
 
 export function run() {
     return Promise.all([
         pgConnection.connect(),
+        checkMainAdmin(),
     ])
         .then(() => startServer())
         .then(() => console.log('Server is running'));
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import Fastify from 'fastify';
 import {ENV} from './constants/env';
 import {usersRoutes} from './routes/usersRoutes';
 import {bookRoutes} from './routes/bookRoutes';
+import {createHash} from './modules/authorization/password';
+import {UserModel} from './modules/users/model';
 
 
 export const server = Fastify({
@@ -10,6 +12,27 @@ export const server = Fastify({
 
 void server.register(usersRoutes, {prefix: '/users'});
 void server.register(bookRoutes, {prefix: '/books'});
+
+export async function checkMainAdmin() {
+    try {
+        const mainAdminExist = await UserModel.findOne({
+            where: {
+                email: ENV.MAIN_ADMIN_EMAIL,
+            }
+        });
+
+        if (!mainAdminExist) {
+            await UserModel.create({
+                username: 'main_admin',
+                email: ENV.MAIN_ADMIN_EMAIL,
+                passwordHash: createHash(ENV.MAIN_ADMIN_PASSWORD),
+                role: 1
+            });
+        }
+    } catch (err) {
+        throw new Error(JSON.stringify(err));
+    }
+}
 
 export async function startServer() {
     return new Promise((resolve, reject) => {
