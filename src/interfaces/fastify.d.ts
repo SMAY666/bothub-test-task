0f771d479c81ab0792fc50c2b764f5ce6992a202
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-08-02 14:29:32 +0700

    Update fastify.d.ts

--- src/interfaces/fastify.d.ts
+++ src/interfaces/fastify.d.ts
@@ -1,8 +1,13 @@
 import {UserInstance} from '../modules/users/model/types';
 import {UserRoles} from '../modules/users/constants';
+import {HookHandlerDoneFunction} from 'fastify';
 
 
 declare module 'fastify' {
+    export interface FastifyInstance {
+        verifyJwt: (req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => void
+        verifyAdmin: (req: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => void
+    }
     export interface FastifyRequest {
         isAuth: boolean
         userId: number
